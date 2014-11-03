184ad926acd7401cf6e605e08ac8ce4bae8aa7e9
Manny <manny.acquah@twentysixdigital.com>
2014-11-03 17:58:31 +0000

    Bower components files and start of jquery swipe function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(function() {
+    //Enable swiping...
+    $(".display-comic").swipe( {
+        //Generic swipe handler for all directions
+        swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+            $(this).text("You swiped " + direction );
+        },
+        //Default is 75px, set to 0 for demo so any distance triggers swipe
+        threshold:0
+    });
+});
